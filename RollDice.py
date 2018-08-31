cd37a333be0e1a1e623d23a0bfd74db04456d6af
Jessica Forsyth <jessicalforsyth@gmail.com>
2018-08-31 12:39:17 -0700

    adds checks for input value errors

--- RollDice.py
+++ RollDice.py
@@ -1,4 +1,8 @@
-"""This program is a game where the user challenges the computer. The computer will roll dice and find the sum, then ask the user to guess the number. If the user chooses the correct number, they will score a point."""
+"""This is a game where the user challenges the computer. 
+The computer will roll dice, find the sum, then ask the 
+user to guess the sum. If the user chooses the 
+correct sum, they will score a point. Otherwise,
+the computer scores."""
 
 from random import randint
 from time import sleep
@@ -17,10 +21,27 @@ def roll_dice(number_of_dice, number_of_sides):
   return rolls
 
 def play_game():
-  number_of_dice = int(raw_input("How many dice do you want to roll? "))
-  number_of_sides = int(raw_input("How many sides does each die have? "))
+  print "The game has begun! \n"
+  while True:
+    try:
+      number_of_dice = int(raw_input("How many dice do you want to roll? "))
+    except ValueError:
+      print "That was not an integer. Try again."
+      continue
+    else:
+      break
+
+  while True:
+    try:
+      number_of_sides = int(raw_input("How many sides does each die have? "))
+    except ValueError:
+      print "That was not an integer. Try again."
+      continue
+    else:
+      break
+
   max_value = number_of_sides * number_of_dice
-  print "The maximum possible value is: %d" % max_value
+  print "\nThe maximum possible value is: %d" % max_value
   
   score = [0, 0]
   play = True
@@ -36,20 +57,21 @@ def play_game():
       rolls = roll_dice(number_of_dice, number_of_sides)
       print "Rolling..."
       sleep(number_of_dice)
-      print "The rolls were %r" % rolls
+      print "Here are the rolls: %r" % rolls
       total_roll = sum(rolls)
-      print "The sum was %d" % total_roll
+      print "The sum is %d.\n" % total_roll
       
+      sleep(2)
       if guess == total_roll:
-        print "You win!"
+        print "You win!\n"
         score[0] += 1
       else:
-        print "The computer won. Try again!"
+        print "The computer won."
         score[1] += 1
       
-    print score
+    print "The score is now: \n YOU: %d \n COMPUTER: %d \n" %(score[0], score[1])
   
-    play_again = raw_input("Do you want to play again? Y or N? \n ... ")
+    play_again = raw_input("Do you want to play again? Y or N? \n ... ").upper()
     if play_again == "N":
       print "Good game!"
       play = False
